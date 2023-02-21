722115017cf20652f30e6388e60c004c0c0089a9
rinkuberde <rinkuberde@gmail.com>
2023-02-21 21:13:55 +0530

    [Rinku] -Welcome Message

---
+++ ProductReviewManagement/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductReviewManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product Review Management using LINQ");
+        }
+    }
+}
